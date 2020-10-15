9dfa9d0edbe9e5b8091d22cb7a97a9950ca6fb42
suchi-dev <suchitrasrinivas10@gmaial.com>
2020-10-15 18:07:10 +0530

    Added java classes to test submitting list of callables through invokeAll method

---
+++ concurrency-basics/src/main/java/com/app/runnables/LoggingProcessor.java
@@ -0,0 +1,17 @@
+package com.app.runnables;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggingProcessor implements Callable<Boolean>{
+
+	@Override
+	public Boolean call() throws Exception {
+		Logger.getLogger(LoggingProcessor.class.getName())
+		.log(Level.INFO, "msg");
+		return true;
+	}
+
+
+}
